5e53cf4ab58018e344da634dc4bb696d6b29c6e5
jayamohanan <jayamohanan96@gmail.com>
2019-04-07 00:04:26 +0530

    s

---
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+
+public class BuildScript{
+    [MenuItem("Jaya/BuildAndroid")]
+	static void PerformBuild()
+    {
+        //Debug.Log("In PerformBuild function")
+        //PlayerSettings.SetApplicationIdentifier(BuildTargetGroup.Android, "com.Mohanan.Jayam");
+        //BuildReport report = 
+        string[] defaultScene = { "Assets/Scenes/SampleScene.unity" };
+        BuildPipeline.BuildPlayer(defaultScene, "./Builds/From_Editor",
+            BuildTarget.Android, BuildOptions.None);
+       // BuildSummary summary = report.summary;
+    }
+}   
